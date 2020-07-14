e845430e6d69d7689f082b810bc5c1089917ac39
Thiago Felippi <thiagocrespo241103@gmail.com>
2020-07-14 18:15:13 -0300

    Added mutations and querys

--- src/graphql/models/post/resolvers.js
+++ src/graphql/models/post/resolvers.js
@@ -4,5 +4,10 @@ export default {
   Query:{
     posts: () => Post.find(),
     post: (_, {id}) => Post.findById(id)
+  },
+
+  Mutation: {
+    createPost: (_, {data}) => Post.create(data),
+    updatePost: (_, {id, data}) => Post.findByIdAndUpdate(id, data, {new: true})
   }
 }
--- src/graphql/models/user/resolvers.js
+++ src/graphql/models/user/resolvers.js
@@ -4,5 +4,10 @@ export default {
   Query:{
     users: () => User.find(),
     user: (_, {id}) => User.findById(id)
+  },
+
+  Mutation: {
+    createUser: (_, {data}) => User.create(data),
+    updateUser: (_, {id, data}) => User.findByIdAndUpdate(id, data, {new: true})
   }
 }
